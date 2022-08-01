2cedbddd9cf479538a19182b55ed66f6c40c55ce
matt70iu <mattadams60@gmail.com>
2022-08-01 01:03:20 +0000

    reboot app heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/tes
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
