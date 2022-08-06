31c7eb7bf000cf571557a631654b7af324782b68
matt70iu <mattadams60@gmail.com>
2022-08-06 13:54:35 +0000

    change settings.py to attempt fix to deploymane error

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
-
+ALLOWED_HOSTS = ["mb2test.herokuapp.com", ".herokuapp.com", "localhost"]
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
